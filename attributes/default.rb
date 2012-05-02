2daa01090306b4b09564050f9ecab556aab60515
Chris Griego <cgriego@gmail.com>
2012-05-01 20:06:06 -0500

    Use upstream attributes to automatically configure downstream attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
-default['scout']['home'] = "/home/scout"
-default['scout']['group'] = "scout"
 default['scout']['user'] = "scout"
+default['scout']['group'] = node['scout']['user']
+default['scout']['home'] = "/home/#{node['scout']['user']}"
 
 default['scout']['gem_packages']
 default['scout']['rvm_ruby_string'] = nil
@@ -10,5 +10,5 @@ default['scout']['key'] = nil
 default['scout']['name'] = nil
 default['scout']['options']
 
-default['scout']['config_dir'] = "/home/scout/.scout"
+default['scout']['config_dir'] = "#{node['scout']['home']}/.scout"
 default['scout']['plugin_public_key'] = nil
