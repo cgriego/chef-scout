1290e53d156fbe30d7150fcf949ab5ca6be01d81
Chris Griego <cgriego@gmail.com>
2012-09-05 17:49:32 -0500

    Fix typo in recipe name

--- recipes/default.rb
+++ recipes/default.rb
@@ -27,6 +27,6 @@
 install_recipe = node['scout']['rvm_ruby_string'] ? "rvm_install" : "gem_install"
 
 include_recipe "scout::user" unless node['scout']['user'] == "root"
-include_recipe "scout::public_private_key" if node['scout']['plugin_public_key']
+include_recipe "scout::plugin_public_key" if node['scout']['plugin_public_key']
 include_recipe "scout::#{install_recipe}"
 include_recipe "scout::cron"
