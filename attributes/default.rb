531add8153a4d12b71664c5e4a58379542db243c
Richard Lee <dlackty@gmail.com>
2013-02-22 23:07:36 +0800

    Bump scout version for default attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@ default['scout']['home'] = "/home/#{node['scout']['user']}"
 default['scout']['gem_packages']
 default['scout']['rvm_ruby_string'] = nil
 default['scout']['rvm_wrapper_prefix'] = "scout"
-default['scout']['version'] = "5.5.4"
+default['scout']['version'] = "5.6.0"
 
 default['scout']['key'] = nil
 default['scout']['name'] = nil
